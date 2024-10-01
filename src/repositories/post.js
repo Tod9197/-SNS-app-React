841e3fc8ca50fbb039a8f35cbeb4dbf087b0898a
Tod9197 <dancein18@yahoo.co.jp>
2024-10-01 18:11:12 +0900

    投稿を表示

--- src/repositories/post.js
+++ src/repositories/post.js
@@ -9,4 +9,19 @@ export const postRepositry = {
     if (error != null) throw new Error(error.message);
     return data[0];
   },
+
+  async find() {
+    const { data, error } = await superbase
+      .from("posts_view")
+      .select("*")
+      .order("created_at", { ascending: false });
+    if (error != null) throw new Error(error.message);
+    return data.map((post) => {
+      return {
+        ...post,
+        userId: post.user_id,
+        userName: post.user_matadata.name,
+      };
+    });
+  },
 };
